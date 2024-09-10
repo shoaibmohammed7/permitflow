e17cb46b6d6da0cb8cbcc2f1375782985ea6058a
Shoaib Mohammed <smohammed7@hawk.iit.edu>
2024-09-09 19:57:31 -0500

    name error

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -7,7 +7,7 @@ import { getServerSession } from "next-auth";
 
 
 
-export default async function(){
+export default async function DashboardPage(){
 
     const  session = await getServerSession(authConfig);
 
